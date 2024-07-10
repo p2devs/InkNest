1cb335b4b5873e899505a4c338103464526ce78b
Pushpender Singh <pushpendersingh694@gmail.com>
2024-07-10 15:38:32 +0530

    chore: add cleanup for event listener to prevent memory leaks

--- src/Components/UIComp/GridList.js
+++ src/Components/UIComp/GridList.js
@@ -1,45 +1,48 @@
-import React, { useEffect, useState, forwardRef, useImperativeHandle, useRef } from "react";
-import { Dimensions, FlatList } from "react-native";
+import React, {
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from 'react';
+import {Dimensions, FlatList} from 'react-native';
 
-const GridList = forwardRef(({ ...rest }, ref) => {
-    const flatListRef = useRef(null);
-    const [numColumns, setNumColumns] = useState(2);
+const GridList = forwardRef(({...rest}, ref) => {
+  const flatListRef = useRef(null);
+  const [numColumns, setNumColumns] = useState(2);
 
-    useEffect(() => {
-        const updateGrid = () => {
-            const width = Dimensions.get('window').width;
-            const newNumColumns = Math.floor(width / 150); // Adjust 150 to desired item width
-            if (newNumColumns <= 2) {
-                setNumColumns(newNumColumns);
-                return;
-            }
-            setNumColumns(newNumColumns - 2);
-        };
+  useEffect(() => {
+    const updateGrid = () => {
+      const width = Dimensions.get('window').width;
+      const newNumColumns = Math.floor(width / 150); // Adjust 150 to desired item width
+      setNumColumns(newNumColumns <= 2 ? newNumColumns : newNumColumns - 2); // Adjust 2 to desired minimum number of columns
+    };
 
-        Dimensions.addEventListener('change', updateGrid); // Set initial number of columns
-        updateGrid(); // Set initial number of columns
+    const dimensionChange = Dimensions.addEventListener('change', updateGrid); // Set initial number of columns
+    updateGrid(); // Set initial number of columns
+    return () => {
+      dimensionChange.remove();
+    };
+  }, []);
 
-    }, []);
+  useImperativeHandle(ref, () => ({
+    scrollToIndex: params => {
+      flatListRef.current.scrollToIndex(params);
+    },
+    scrollToOffset: params => {
+      flatListRef.current.scrollToOffset(params);
+    },
+    // Add more functions as needed
+  }));
 
-    useImperativeHandle(ref, () => ({
-        scrollToIndex: (params) => {
-            flatListRef.current.scrollToIndex(params);
-        },
-        scrollToOffset: (params) => {
-            flatListRef.current.scrollToOffset(params);
-        },
-        // Add more functions as needed
-    }));
-
-
-    return (
-        <FlatList
-            ref={flatListRef}
-            numColumns={numColumns}
-            key={numColumns}
-            {...rest}
-        />
-    )
+  return (
+    <FlatList
+      ref={flatListRef}
+      numColumns={numColumns}
+      key={numColumns}
+      {...rest}
+    />
+  );
 });
 
 export default GridList;
