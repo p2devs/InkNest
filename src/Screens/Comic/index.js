21543efe15635fd8a06a3c4df8dc9d7871e2f28c
Pushpender Singh <pushpendersingh694@gmail.com>
2024-07-27 22:34:08 +0530

    fix: crash happening due to wrong import

--- src/Screens/Comic/index.js
+++ src/Screens/Comic/index.js
@@ -1,4 +1,4 @@
-export { Home } from './Home/Home';
-export { ComicDetails } from './Details/ComicDetails';
-export { ComicBook } from './Book/ComicBook';
-export { LocalComic } from './LocalComic/index';
+export {Home} from './Home/Home';
+export {ComicDetails} from './Details/ComicDetails';
+export {ComicBook} from './Book/ComicBook';
+export {LocalComic} from './LocalComic/index';
