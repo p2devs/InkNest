f1aa7830a36b54667518ee0a3ce2d7cb7a5da609
Pushpender Singh <pushpendersingh694@gmail.com>
2025-10-09 02:00:29 +0530

    refactor: enhance ComicBookmarks component layout

--- src/Screens/Comic/Bookmarks/index.js
+++ src/Screens/Comic/Bookmarks/index.js
@@ -1,14 +1,52 @@
 import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 import {Bookmarks} from './Bookmarks';
+import Header from '../../../Components/UIComp/Header';
+import {goBack} from '../../../Navigation/NavigationService';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {heightPercentageToDP} from 'react-native-responsive-screen';
 
 export function ComicBookmarks({navigation}) {
   return (
     <SafeAreaView style={styles.container} edges={['top']}>
+      <Header
+        style={{
+          width: '100%',
+          height: heightPercentageToDP('4%'),
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingHorizontal: 12,
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            goBack();
+          }}>
+          <Ionicons
+            name="arrow-back"
+            size={24}
+            color="#fff"
+            style={{marginRight: 10, opacity: 0.9}}
+          />
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: '700',
+            color: '#fff',
+            opacity: 0.9,
+          }}>
+          Bookmarks
+        </Text>
+        <View style={{flex: 0.1}} />
+      </Header>
       <View style={styles.container}>
-        <View style={styles.tabBarContainer}>
-          <Text style={styles.tabText}>Bookmarks</Text>
-        </View>
         <Bookmarks navigation={navigation} />
       </View>
     </SafeAreaView>
